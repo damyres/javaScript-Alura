b6adf1bf7ed41a3ddfe8a39f3a126f69f310ceb9
Damyres <damyresmaciell@gmail.com>
2024-06-24 15:53:24 -0300

    Adicionar função que impede recarregar página após digitar o texto

--- crud.js
+++ crud.js
@@ -2,8 +2,13 @@
 
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task');
 const formAdicionarTarefa = document.querySelector('.app__form-add-task');
+const textArea = document.querySelector('.app__form-textarea');
 
 btnAdicionarTarefa.addEventListener('click', ()=> {
     formAdicionarTarefa.classList.toggle('hidden');
+});
+
+formAdicionarTarefa.addEventListener('submit', (evento) => {
+    evento.preventDefault();
 
 });
