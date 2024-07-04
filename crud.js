dbbe7017a88d1b2d54ee20e78de418e7e2a0aa4a
Damyres <damyresmaciell@gmail.com>
2024-07-04 20:28:34 -0300

    Adicionar funcionalidade para que quando a cronometro for finalizado o formulario mude para cor verde e fique desabilitado para edição, pois a tarefa foi finalizada

--- crud.js
+++ crud.js
@@ -9,6 +9,7 @@ const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-ta
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
 let tarefaSelecionada = null
+let liTarefaSelecionada 
 
 const limparFormulario = () => {
     textarea.value = '';
@@ -64,9 +65,11 @@ function criarElementoTarefa(tarefa) {
         if (tarefaSelecionada == tarefa) {
             paragrafoDescricaoTarefa.textContent = ''
             tarefaSelecionada = null
+            liTarefaSelecionada = null
             return
         }
         tarefaSelecionada = tarefa
+        liTarefaSelecionada = li
         paragrafoDescricaoTarefa.textContent = tarefa.descricao
         document.querySelectorAll('.app__section-task-list-item-active')
             .forEach(elemento => {
@@ -101,3 +104,12 @@ tarefas.forEach(tarefa => {
     const elementoTarefa = criarElementoTarefa(tarefa);
     ulTarefas.append(elementoTarefa);
 });
+document.addEventListener('focoFinalizado', () => {
+    if (tarefaSelecionada && liTarefaSelecionada){
+        liTarefaSelecionada.classList.remove('app__section-task-list-item-active')
+        liTarefaSelecionada.classList.add('app__section-task-list-item-complete')
+        liTarefaSelecionada.querySelector('button').setAttribute('disabled', 'desabled')
+
+    }
+
+})
